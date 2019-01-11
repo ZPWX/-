256c2cd28bbafc33862b88cfd66888cb29ce61dc
zpwx <3157188714@qq.com>
2019-01-11 20:03:07 +0800

    learn python

---
+++ 运算符.py
@@ -0,0 +1,19 @@
+#字符串间比较
+name1 = "gxtao"
+name2 = "gxtao"
+print(name1 < name2)
+#字符串间比较以ASCII为具
+name1 = "0"
+name2 = "9"
+print(name1 < name2)
+
+#真值表比较
+office = True
+money = True
+print(office and money)
+print(office + money) 
+
+train = True
+bus = False
+print(train or bus)
+
