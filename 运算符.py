07a0050c4f1d99b8d08e695707f504907f4de862
zpwx <3157188714@qq.com>
2019-01-11 21:08:01 +0800

    ifelse

--- 运算符.py
+++ 运算符.py
@@ -1,13 +1,12 @@
-#字符串间比较
+
 name1 = "gxtao"
 name2 = "gxtao"
 print(name1 < name2)
-#字符串间比较以ASCII为具
+
 name1 = "0"
 name2 = "9"
 print(name1 < name2)
 
-#真值表比较
 office = True
 money = True
 print(office and money)
@@ -17,3 +16,10 @@ train = True
 bus = False
 print(train or bus)
 
+score = 100
+if score == 100:
+    print("A")
+elif score >= 60 and score < 100:
+    print("B")
+else:
+    print("C")
