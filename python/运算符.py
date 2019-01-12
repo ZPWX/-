34d5066e2cee264993936dca947171e0bbd037bd
zpwx <3157188714@qq.com>
2019-01-12 10:08:29 +0800

    pythonfile

---
+++ python/运算符.py
@@ -0,0 +1,25 @@
+
+name1 = "gxtao"
+name2 = "gxtao"
+print(name1 < name2)
+
+name1 = "0"
+name2 = "9"
+print(name1 < name2)
+
+office = True
+money = True
+print(office and money)
+print(office + money) 
+
+train = True
+bus = False
+print(train or bus)
+
+score = 100
+if score == 100:
+    print("A")
+elif score >= 60 and score < 100:
+    print("B")
+else:
+    print("C")
