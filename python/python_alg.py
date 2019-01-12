a50848ca8147d13821e7a56266982f84bf773d71
zpwx <3157188714@qq.com>
2019-01-12 12:29:40 +0800

    ifwhile

--- python/python_alg.py
+++ python/python_alg.py
@@ -5,3 +5,11 @@ while num <= 100:
         sum = sum + num
     num += 1
 print(sum)
+
+i = 100
+while i <= 999:
+    a = i // 100
+    b = i % 10
+    if a == b:
+        print(i,end="-")
+    i += 1
