672db7265522cb1a87fe4615dc3be8427602fe0e
zpwx <3157188714@qq.com>
2019-01-12 11:02:49 +0800

    add pythonalg

--- python/运算符.py
+++ python/运算符.py
@@ -23,3 +23,26 @@ elif score >= 60 and score < 100:
     print("B")
 else:
     print("C")
+
+week = int(input("input a number/string"))
+if week == 1:
+    print("A")
+elif week == 2:
+    print("B")
+
+x = 0
+if x >= 0:
+    print("这个是正数")
+    if x % 2 == 0:
+        print("这个是偶数")
+    else:
+        print("这个是奇数")
+else:
+    print("这个是负数")
+
+i = 0
+while i < 5:
+    print("hello python")
+    i += 1
+
+
