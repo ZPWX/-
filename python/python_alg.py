5603170bd61f2e14a5cf2f3101ae2f147dad769f
zpwx <3157188714@qq.com>
2019-01-14 20:09:00 +0800

    aplot

--- python/python_alg.py
+++ python/python_alg.py
@@ -13,15 +13,15 @@ while i <= 999:
     if a == b:
         print(i)
     i += 1
-'''
-j = 1
-while j <= 5:
-    i = 1
-    while i <= 3:
-        print(i)
-        i += 1
-    j += 1
-'''
+
+#j = 1
+#while j <= 5:
+#    i = 1
+#    while i <= 3:
+#        print(i)
+#        i += 1
+#    j += 1
+
 
 j = 1
 while j <= 5:
---
+++ python/python_fun.py
@@ -0,0 +1,12 @@
+def sum(a):
+    #用于计算指定数字和#
+    i = 1
+    sums = 0
+    while i <= a:
+        sums += i
+        i += 1
+    print(sums)
+
+sum(10)
+
+
