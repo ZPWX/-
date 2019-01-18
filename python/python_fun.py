c3e948b06832d18da060ea57f9b5a2433cfffded
zpwx <3157188714@qq.com>
2019-01-18 16:52:37 +0800

    类

--- python/python_fun.py
+++ python/python_fun.py
@@ -39,3 +39,32 @@ class Card:
 c = Card()
 c.set_pwd("gxtao")
 print(c.get_pwd())
+"""
+# 类变量
+class Cat1:
+    subject = "cat"
+    def _init_(self, type, name):
+        self.__type = type
+        self.__name = name
+    def get_type(self):
+        # print("bosicat")
+        return self.__type
+    def set_type(self,type):
+        self.__type = type
+    def get_name(self):
+        return self.__name
+    def set_name(self,name):
+        self.__name = name
+cat1 = Cat1("bosicat","shortcat")
+cat2 = Cat1("bosicat","hadsome")
+"""
+# 类方法
+class Chinese:
+    country = "China"
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+    @classmethod
+    def show_country(cls): #类方法的定义
+        print("i'am chinese")
+
